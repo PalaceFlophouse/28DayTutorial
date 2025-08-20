f4a5be0e1d905848be17fadaebc46a919de63524
Brandon Shaffer <brandonrussellshaffer@gmail.com>
2025-08-20 19:47:37 -0400

    Initial Commit

---
+++ src/main/java/com/palaceflophouse/twoeightdaytutorial/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.palaceflophouse.twoeightdaytutorial;
+
+public class HelloWorldBean {
+
+    String message;
+
+    public HelloWorldBean(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
